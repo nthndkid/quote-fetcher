fd4f0c68ab4a1d363946459b39f4ef0a2cc435ea
nthndkid <143003322+nthndkid@users.noreply.github.com>
2025-05-21 17:56:01 +0800

    added quotable api

--- script.js
+++ script.js
@@ -4,15 +4,15 @@
 const toggleBtn = document.getElementById('theme-toggle');
 const root = document.documentElement;
 
-// load theme 
-if (localStorage.getItem('theme') === 'dark'){
-    root.classList.add('dark');
-} else {
-  root.classList.remove('dark');
-}
-
 // toggle theme
 if (toggleBtn) {
+    // load theme 
+    if (localStorage.getItem('theme') === 'dark'){
+        root.classList.add('dark');
+    } else {
+    root.classList.remove('dark');
+    }
+
     toggleBtn.addEventListener('click', () => {
         root.classList.toggle('dark');
         
@@ -25,3 +25,29 @@ if (toggleBtn) {
     })
 }
 
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const getQuoteBtn = document.getElementById("get-quote");
+
+if (getQuoteBtn) {
+    getQuoteBtn.addEventListener("click", async() => {
+        getQuoteBtn.disabled = true;
+        getQuoteBtn.textContent = "Loading...";
+
+        try {
+            const response = await fetch("https://api.quotable.io/random");
+            const data = await response.json();
+            console.log(data);
+
+            quoteText.textContent = data.content;
+            quoteAuthor.textContent = `- ${data.author}`;
+        } catch (error) {
+            console.log(error);
+            quoteText.textContent = "Failed to fetch quote, Please try again later!";
+            quoteAuthor.textContent = "-----";
+        }
+
+        getQuoteBtn.textContent = "Get Quote";
+        getQuoteBtn.disabled = false;
+    });
+}
